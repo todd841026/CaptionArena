c77eedd671eed50ad9c8a657837106e17dd316bb
hutingji <hutingji@zhijin.com>
2016-08-30 19:33:41 +0800

    提交修改操作

--- caption-arena/config/web-extension/site-webscripts/org/alfresco/components/videotest/video.get.html.ftl
+++ caption-arena/config/web-extension/site-webscripts/org/alfresco/components/videotest/video.get.html.ftl
@@ -33,16 +33,12 @@
         <li><a href="#">返回</a></li>
         <li class="active">Mad Max</li>
     </ol>
-    <input type="hidden" value="${srtId}" id="in3"/>
     <div id="video-player">
         <div class="video-background">
             <video id="video-js" class="video-js vjs-default-skin" controls preload="auto" width="1024" height="420"
                    poster="${url.context}/res/components/video/images/pic.png" data-setup={"example_option":true}>
-                <source src="http://localhost:8080/caption/proxy/alfresco/api/external/node/content/${nodeRef}"
-                        type="video/mp4"/>
-                <track id="video-srt" kind="captions"
-                       src="http://localhost:8080/caption/proxy/alfresco/api/external/node/content/${srtId}"
-                       srclang="it" label="Italian" default/>
+                <source src="" type="video/mp4"/>
+                
                 <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web
                     browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5
                         video</a></p>
@@ -57,10 +53,10 @@
             <div class="editarena">
                 <input class="editInput" type="text" id="editInputCa" placeholder="请点击此处编辑字幕" onfocus="focusText()"
                        id="time2"/>
-                <button class="btn btn-default" id="btn_madeCa" onclick="upjson()">确定</button>
+                <button class="btn btn-default" id="btn_madeCa" id="upjson">确定</button>
             </div>
             <div class="center">
-                <button class="btn btn-default submit">提交</button>
+                <input class="btn btn-default submit" onclick="onSubmit()" id="submit" value="提交">
                 <p>注: 点击“提交”按钮后，您之前所有“确定”过的字幕都将一并提交。</p>
             </div>
         </div>
--- caption-arena/web/components/video/captionvideo.js
+++ caption-arena/web/components/video/captionvideo.js
@@ -10,113 +10,185 @@ var contentsrtitem = null;
 var isPlay = true;
 var srtx = null;
 
+var video = new Object();
+
 $(document).ready(function() {
-	srtx = new SRT();
-	_srtId = $("#in3").val();
-	player = videojs('#video-js');
-
-	$.ajax({
-		url : "/caption/proxy/alfresco/" + "api/external/node/contentsrt/"+_srtId,
-		cache : false,
-		success : function(response) {
-			srt = response;
-			myData = srtx.parse(srt);
-			player.on('timeupdate',function() {
-				timeupdate = player.currentTime();
-				for ( var i in myData) {
-					var ob = myData[i];
-					var obStartTime = parseFloat(ob["time"]["start"]);
-					var obEndTime = parseFloat(ob["time"]["end"]);
-					// 要添加项的时间段正好在循环项时间段之间
-					$("#editInputCa").val("");
-					if (timeupdate >= obStartTime && timeupdate <= obEndTime) {
-						currentid = i;
-						var content = ob["content"].join(",");
-						contentsrtitem = content;
-						$("#editInputCa").val(contentsrtitem);
-						return;
-					}
-				}
-			});
-			//开始或恢复播放
-			player.on('play', function() {
-				console.log('开始/恢复播放');
-				isPlay = true;
-			});
-			// 暂停播放
-			player.on('pause', function() {
-				console.log('暂停播放');
-				isPlay = false;
-			});
-			var upjson = function() {
-
-				if (isPlay) {
-					console.log("必须暂停才能编辑")
-				} else {
-					console.log('update:'+currentid);
-					editData(Number(currentid) + 1);
-				}
-			}
-		}
-	});
+	var nodeRef = Alfresco.util.getQueryStringParameter('nodeRef');
+	$("#video-js").find('source').attr("src",linkVideo(nodeRef));
+		
+    srtx = new SRT();
+    player = videojs('#video-js');
+    
+    $.ajax({
+        url : Alfresco.constants.PROXY_URI + "api/external/caption/getSrtIdByVideoId?videoId="+nodeRef,
+        cache : false,
+        success : function(response) {
+        	console.log(response);
+        	video.srtId = response.srtId;
+        	$("#video-js").find('source').after("<track id='video-srt' src='"+linkVideoSrt(video.srtId)+"'srclang='en' label= 'English' kind='caption' default>");
+        	videoSrtContent(video.srtId);
+        }
+    });
+    
+  
 });
 
-function addData() {
-	var c = myData.length;
-	id = ++c;
-	var endTime = parseFloat(1);
-	var startTime = endTime - parseFloat(1);
-	var duration = Math.round((endTime - startTime) * 1000) / 1000;
-	var timeObject = {
-		"start" : startTime,
-		"end" : endTime,
-		"duration" : duration
-	};
-	var item = {
-		"id" : id,
-		"time" : timeObject,
-		"content" : [ content ]
-	};
-	srtx.addSrt(myData, item);
-};
-function lefttime() {
-	player.pause();
-	var currentTime = player.currentTime();
-	currentTime = Math.round(currentTime);
-	var newTime = 0;
-	if (currentTime >= 1) {
-		newTime = currentTime - 1;
-	}
-	player.currentTime(newTime);
-};
-function righttime() {
-	player.pause();
-	var currentTime = player.currentTime();
-	currentTime = Math.round(currentTime);
-	var newTime = 0;
-
-	newTime = currentTime + 1;
-	player.currentTime(newTime);
-};
-function editData(id) {
-	var ob = myData[id-1];
-	var obStartTime = parseFloat(ob["time"]["start"]);
-	var obEndTime = parseFloat(ob["time"]["end"]);
-	if (timeupdate >= obStartTime && timeupdate <= obEndTime) {
-		srtx.editSrt(myData, id, $("#editInputCa").val());
-	}else{
-		addData();
-	}
-};
-
-function deleteData(id) {
-	srtx.delteSrt(myData, id);
-};
-
-function focusText() {
-	player.pause();
+//	根据nodeRef获取srtId
+function linkVideo(nodeRef){	
+	var link = Alfresco.constants.PROXY_URI + "api/external/node/content/" + nodeRef;
+	return link;	
 }
 
+//	根据srtId获取srt值
+function linkVideoSrt(srtId){
+	var srtlink = Alfresco.constants.PROXY_URI + "api/external/node/contentsrt/" + srtId;
+	return srtlink;
+}
+
+function videoSrtContent(srtId){
+	$.ajax({
+        url : Alfresco.constants.PROXY_URI + "api/external/node/contentsrt/" + srtId,
+        cache : false,
+        success : function(response) {
+            srt = response;
+            myData = srtx.parse(srt);
+            player.on('timeupdate',function() {
+                timeupdate = player.currentTime();
+                for ( var i in myData) {
+                    var ob = myData[i];
+                    var obStartTime = parseFloat(ob["time"]["start"]);
+                    var obEndTime = parseFloat(ob["time"]["end"]);
+                    // 要添加项的时间段正好在循环项时间段之间
+                    $("#editInputCa").val("");
+                    if (timeupdate >= obStartTime && timeupdate <= obEndTime) {
+                        currentid = i;
+                        var content = ob["content"].join(",");
+                        contentsrtitem = content;
+                        $("#editInputCa").val(contentsrtitem);
+                        return;
+                    }
+                }
+            });
+            //开始或恢复播放
+            player.on('play', function() {
+                console.log('开始/恢复播放');
+                isPlay = true;
+            });
+            // 暂停播放
+            player.on('pause', function() {
+                console.log('暂停播放');
+                isPlay = false;
+            });
+            $("#upjson").click(function(){
+            	upjson();
+            });
+            
+            function upjson() {
+
+                if (isPlay) {
+                    console.log("必须暂停才能编辑")
+                } else {
+                    console.log('update:'+currentid);
+                    editData(Number(currentid) + 1);
+                }
+            };
+        }
+    });
+}
+
+//	增加数据
+function addData() {
+    var c = myData.length;
+    id = ++c;
+    var endTime = parseFloat(1);
+    var startTime = endTime - parseFloat(1);
+    var duration = Math.round((endTime - startTime) * 1000) / 1000;
+    var timeObject = {
+        "start" : startTime,
+        "end" : endTime,
+        "duration" : duration
+    };
+    var item = {
+        "id" : id,
+        "time" : timeObject,
+        "content" : [ content ]
+    };
+    srtx.addSrt(myData, item);
+}
+
+//	时间左移动
+function lefttime() {
+    player.pause();
+    var currentTime = player.currentTime();
+    currentTime = Math.round(currentTime);
+    var newTime = 0;
+    if (currentTime >= 1) {
+        newTime = currentTime - 1;
+    }
+    player.currentTime(newTime);
+}
+
+//	时间右移动
+function righttime() {
+    player.pause();
+    var currentTime = player.currentTime();
+    currentTime = Math.round(currentTime);
+    var newTime = 0;
+
+    newTime = currentTime + 1;
+    player.currentTime(newTime);
+}
+
+//  编辑字幕数据
+function editData(id) {
+    var ob = myData[id-1];
+    var obStartTime = parseFloat(ob["time"]["start"]);
+    var obEndTime = parseFloat(ob["time"]["end"]);
+    if (timeupdate >= obStartTime && timeupdate <= obEndTime) {
+        srtx.editSrt(myData, id, $("#editInputCa").val());
+    }else{
+        addData();
+    }
+}
+
+//	删除数据
+function deleteData(id) {
+    srtx.delteSrt(myData, id);
+}
+
+// 	编辑输入框时，视频暂停
+function focusText() {
+    player.pause();
+}
+
+
+// 提交修改数据
 function onSubmit(){
-	console(player.pause());
+    console.log("提交");
+    
+    var updata = {
+    	"srtId": video.srtId,
+    	"srtContent": JSON.stringify(myData)
+    }
+    Alfresco.util.Ajax.request(
+            {
+               method: Alfresco.util.Ajax.POST,
+   			   url: Alfresco.constants.PROXY_URI+ "api/external/caption/updateSrt",
+   			   requestContentType : "application/json",
+   			   dataObj: updata,
+//   			   requestContentType : "application/json",
+//   			   responseContentType : "application/json",
+   				successCallback:
+   				{
+   					fn: function(){
+   						console.log("提交成功");
+   					},
+   					scope: this
+   				},
+   				failureCallback:
+   				{
+   					fn: this.onArchiveRefresh_success,
+   					scope: this
+   				}
+   			});
 }
